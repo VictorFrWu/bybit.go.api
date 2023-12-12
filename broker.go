3a13a133553034dbd3ddc55b7a09e55ae06cc933
wuhewuhe <dachichiwuhe@gmail.com>
2023-12-13 00:16:15 +0100

    add ping interval and maxAlive time option

--- broker.go
+++ broker.go
@@ -18,7 +18,29 @@ func (s *BrokerServiceClient) GetBrokerEarning(ctx context.Context, opts ...Requ
 	}
 	r := &request{
 		method:   http.MethodGet,
-		endpoint: "/v5/broker/earning-record",
+		endpoint: "/v5/broker/earnings-info",
+		secType:  secTypeSigned,
+	}
+	r.setParams(s.params)
+	data, err := s.c.callAPI(ctx, r, opts...)
+	if err != nil {
+		return nil, err
+	}
+	res = new(ServerResponse)
+	err = json.Unmarshal(data, res)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+func (s *BrokerServiceClient) GetBrokerAccountInfo(ctx context.Context, opts ...RequestOption) (res *ServerResponse, err error) {
+	if err = handlers.ValidateParams(s.params); err != nil {
+		return nil, err
+	}
+	r := &request{
+		method:   http.MethodGet,
+		endpoint: "/v5/broker/account-info",
 		secType:  secTypeSigned,
 	}
 	r.setParams(s.params)
